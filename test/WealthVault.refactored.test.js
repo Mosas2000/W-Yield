3949613a2f8f9ad92c465ef92e88d95dca6b39d0
0xMosas <tosinolosasa@gmail.com>
2026-01-26 21:20:10 +0100

    test: refactored tests with helpers

---
+++ test/WealthVault.refactored.test.js
@@ -0,0 +1,32 @@
+const { expect } = require("chai");
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { vaultFixture, vaultWithDepositsFixture } = require("./helpers/fixtures");
+const { setupUser, deposit, withdraw } = require("./helpers/utils");
+const { MIN_DEPOSIT, MAX_DEPOSIT } = require("./helpers/constants");
+
+describe("WealthVault - Refactored", function () {
+    describe("Deployment", function () {
+        it("Should deploy correctly", async function () {
+            const { vault, usdc } = await loadFixture(vaultFixture);
+            expect(await vault.usdc()).to.equal(usdc.address);
+        });
+    });
+    
+    describe("Deposits", function () {
+        it("Should handle deposit", async function () {
+            const { vault, usdc, user1 } = await loadFixture(vaultFixture);
+            await setupUser(usdc, vault, user1, MIN_DEPOSIT);
+            await deposit(vault, user1, MIN_DEPOSIT);
+            expect(await vault.balanceOf(user1.address)).to.equal(MIN_DEPOSIT);
+        });
+    });
+    
+    describe("Withdrawals", function () {
+        it("Should handle withdrawal", async function () {
+            const { vault, user1 } = await loadFixture(vaultWithDepositsFixture);
+            const shares = await vault.balanceOf(user1.address);
+            await withdraw(vault, user1, shares);
+            expect(await vault.balanceOf(user1.address)).to.equal(0);
+        });
+    });
+});
